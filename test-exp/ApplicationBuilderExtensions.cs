a1e63935c315ae183239aba298a9d9890f0a97ac
Lucas dos Santos Alves <30706052+lucasdsalves@users.noreply.github.com>
2022-12-02 11:42:39 -0300

    initial

---
+++ test-exp/ApplicationBuilderExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace test_exp
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder AddGlobalErrorHandler(this IApplicationBuilder applicationBuilder)
+            => applicationBuilder.UseMiddleware<GlobalErrorHandlingMiddleware>();
+    }
+}
---
+++ test-exp/GlobalErrorHandlingMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace test_exp
+{
+    public class GlobalErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            HttpStatusCode status;
+
+            var exceptionType = exception.GetType();
+
+            if (exceptionType == typeof(InvalidOperationException))
+            {
+                status = HttpStatusCode.NotFound;
+            }
+            else if (exceptionType == typeof(PasswordIncorrectException))
+            {
+                status = HttpStatusCode.Unauthorized;
+            }
+            else
+            {
+                status = HttpStatusCode.InternalServerError;
+            }
+
+            var exceptionResult = JsonSerializer.Serialize(new { error = exception.Message, exception.StackTrace });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)status;
+
+            return context.Response.WriteAsync(exceptionResult);
+        }
+    }
+}
